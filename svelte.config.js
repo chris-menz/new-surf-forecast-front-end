871b3e9a532e82010a7bbb68e8c019d7ca2375aa
chris-menz <cmenz8525@gmail.com>
2022-02-27 01:08:33 -0500

    new commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import vercel from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
   },
 };
 
