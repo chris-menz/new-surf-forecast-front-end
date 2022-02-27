2f32c1f48f9abc79a5d1e0d853df623cbc462b83
chris-menz <cmenz8525@gmail.com>
2022-02-27 14:50:52 -0500

    headers

--- src/lib/utils/axiosConfig.js
+++ src/lib/utils/axiosConfig.js
@@ -1,8 +1,6 @@
 export const axiosConfig = {
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
   withCredentials: true,
 };
