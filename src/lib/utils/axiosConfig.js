13c11182367e57f58338f56c321f29b559458beb
chris-menz <cmenz8525@gmail.com>
2022-02-27 15:19:48 -0500

    headers

--- src/lib/utils/axiosConfig.js
+++ src/lib/utils/axiosConfig.js
@@ -1,7 +1,6 @@
 export const axiosConfig = {
   headers: {
     "Content-Type": "application/json",
-    Origin: "https://condescending-hoover-f550c9.netlify.app",
   },
   withCredentials: true,
 };
