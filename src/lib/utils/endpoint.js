8fc02f0869000cd6434099811d906d790e39086d
chris-menz <cmenz8525@gmail.com>
2022-02-27 14:10:41 -0500

    url

--- src/lib/utils/endpoint.js
+++ src/lib/utils/endpoint.js
@@ -1,2 +1,2 @@
-// export const endpoint = "http://127.0.0.1:8080/api";
-export const endpoint = "http://142.93.127.161:8080/api";
+export const endpoint = "http://127.0.0.1:8080/api";
+// export const endpoint = "http://142.93.127.161:8080/api";
